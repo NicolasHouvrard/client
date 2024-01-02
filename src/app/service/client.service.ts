2ec7c7cb422f4e17b9cf42e782285a3f36ecce23
NicolasHouvrard <nicolas@logicielpourtous.com>
2024-01-02 14:42:35 +0100

    modified:   src/app/service/client.service.ts

--- src/app/service/client.service.ts
+++ src/app/service/client.service.ts
@@ -1,49 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Client} from '../interface/client';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Client } from '../interface/client';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { BehaviorSubject, Observable, tap, map, catchError,  } from 'rxjs';
+import { BehaviorSubject, Observable, tap, map, catchError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientService {
-
-  clients$: BehaviorSubject<Client[] | null >= new BehaviorSubject<Client[] | null>([]);
+  clients$: BehaviorSubject<Client[] | null> = new BehaviorSubject<
+    Client[] | null
+  >([]);
 
   constructor(private http: HttpClient) {
     this.liste();
-
-   }
+  }
 
   public liste(): Observable<Client[] | null> {
     return this.http
       .get<Client[] | null>('https://localhost:8000/api/clients', {
         headers: new HttpHeaders({
-          'Accept':  'application/ld+json'
-        })
+          Accept: 'application/ld+json',
+        }),
       })
       .pipe(
         tap((res: any) => {
-        console.log(res);
-        let  totalItems = res['hydra:totalItems']
-        let   members =  res['hydra:member']
+          console.log(res);
+          let totalItems = res['hydra:totalItems'];
+          let members = res['hydra:member'];
 
-        this.clients$.next(members)
-
+          this.clients$.next(members);
         })
-      )
-
+      );
   }
   public add(client: Client): Observable<any> {
-    const headers = { 'content-type': 'application/ld+json'}
-    const body=JSON.stringify(client);
+    const headers = { 'content-type': 'application/ld+json' };
+    const body = JSON.stringify(client);
+    
+    console.log('body', body);
 
-    console.log("body", body)
-
-    return this.http.post('https://localhost:8000/api/clients',
-                          body,
-                          {'headers':headers} )
-
+    return this.http.post('https://localhost:8000/api/clients', body, {
+      headers: headers,
+    });
   }
 }
